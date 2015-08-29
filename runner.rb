241bd9057c935fe709e504a3b3db56148965f7a8
Matías Maximiliano Simone <matias.simone@live.com.ar>
2015-08-29 20:07:49 -0300

    Update runner.rb

--- runner.rb
+++ runner.rb
@@ -1,24 +1,29 @@
 require_relative './original'
 require_relative './extend'
 
+MyTetris.new
+  mainLoop
+  
+=begin
 def runTetris
   Tetris.new 7
   mainLoop
 end
 
 def runTetrisExtend
-  TetrisExtend.new
+  MyTetris.new
   mainLoop
 end
 
 if ARGV.count == 0
-  TetrisExtend
+  runTetrisExtend
 elsif ARGV.count != 1
   puts "usage: runner.rb [enhanced | original]"
 elsif ARGV[0] == "enhanced"
-  TetrisExtend
+  runTetrisExtend
 elsif ARGV[0] == "original"
   runTetris
 else
   puts "usage: runner.rb [enhanced | original]"
 end
+=end
